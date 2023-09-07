60179e89abc12f258ea2144ca9594426afc0ac77
Developer Baboo <babooheerani@gmail.com>
2023-09-07 18:58:30 +0500

    added twilland css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link} from "react-router-dom";
+import { Home } from "./components/Home";
+import { SkillIndex } from "./components/skills/SkillIndex";
+import { SkillCreate } from "./components/skills/SkillCreate";
+import { SkillEdit } from "./components/skills/SkillEdit";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return <div className="bg-slate-200" >
+    {/* https://www.youtube.com/watch?v=NS0RI2CfMLE&t=8s */}
+    40:40
+  </div>;
 }
 
 export default App;
---
+++ src/components/skills/SkillIndex.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SkillIndex = () => {
+  return (
+    <div>SkillIndex</div>
+  )
+}
