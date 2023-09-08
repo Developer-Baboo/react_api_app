4635a2fdcae61cb799f1f34de4d05b60bc5792f4
Developer Baboo <babooheerani@gmail.com>
2023-09-08 11:12:51 +0500

    fetched data from apis

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import { Home } from "./components/Home";
 import { SkillIndex } from "./components/skills/SkillIndex";
 import { SkillCreate } from "./components/skills/SkillCreate";
 import { SkillEdit } from "./components/skills/SkillEdit";
+import React from "react";
 
 function App(){
   return <div className="bg-slate-200" >
-    {/* https://www.youtube.com/watch?v=NS0RI2CfMLE&t=8s */}
-    40:40
+    <div className="max-2-7xl max-auto min-h-screen">
+        <nav>
+          <ul className="flex" >
+             <li className="m-2 p-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">
+              <Link to="/">Home</Link>
+             </li>
+              <li className="m-2 p-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">
+              <Link to="/skills">Skills</Link>
+             </li>
+          </ul>
+        </nav>
+        <Routes>
+            <Route path="/" element={ <Home />} />
+            <Route path="/skills" element={ <SkillIndex />} />
+            <Route path="/skills/create" element={ <SkillCreate />} />
+            <Route path="/skills/:id/edit" element={ <SkillEdit />} />
+        </Routes>
+    </div>
   </div>;
 }
 
--- src/components/skills/SkillIndex.jsx
+++ src/components/skills/SkillIndex.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import React from 'react';
 
 export const SkillIndex = () => {
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const getSkills = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/v1/skills');
+        setSkills(response.data.data);
+      } catch (error) {
+        console.error('Error fetching skills:', error);
+      }
+    };
+    getSkills();
+  }, []);
+
   return (
-    <div>SkillIndex</div>
-  )
-}
+    <div className="mt-12">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Slug
+              </th>
+              <th scope="col" className="px-6 py-3"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {skills.map((skill) => (
+              <tr key={skill.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td className="py-4 px-6">{skill.name}</td>
+                <td className="py-4 px-6">{skill.slug}</td>
+                <td className="py-4 px-6">Edit/delete</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
